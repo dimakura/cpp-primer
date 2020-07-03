58eaeb65c845de9e769029d25b78dc006b315304
Dimitri Kurashvili <dimakura@gmail.com>
2020-07-03 22:48:40 +0400

    References

---
+++ 02/prog4.cpp
@@ -0,0 +1,22 @@
+// References (pp. 50-51)
+#include <iostream>
+#include <ostream>
+
+int main() {
+  // ex 2.16
+  // int i = 0, &r1 = i;
+  // double d = 0, &r2 = d;
+  // r2 = 3.14159;
+  // r2 = r1;
+  // i = r2;
+  // r1 = d;
+  // std::cout << i << " " << d << std::endl;
+
+  // ex 2.17
+  int i, &ri = i;
+  i = 5;
+  ri = 10;
+  std::cout << i << " " << ri << std::endl;
+
+  return 0;
+}
