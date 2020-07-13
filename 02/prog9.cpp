2a46976c947478c8e886c3395992ecda56a1ba48
Dimitri Kurashvili <dimakura@gmail.com>
2020-07-13 22:49:52 +0400

    More of decltype

--- 02/prog9.cpp
+++ 02/prog9.cpp
@@ -6,5 +6,17 @@ int main() {
   decltype(cj) y = x;
   // decltype(cj) z; // ERROR: initialization is required
 
+  int i = 42, *p = &i, &r = i;
+  decltype(r + 0) b;  // int
+  decltype(*p) c = i; // int&
+
+  // Exercise 2.36
+  int a1 = 3, b1 = 4;
+  decltype(a1) c1 = a1;
+  decltype((b1)) d1 = a1;
+  ++c1;
+  ++d1;
+  std::cout << a1 << " " << b1 << " " << c1 << " " << d1 << std::endl;
+
   return 0;
 }
