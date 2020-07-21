1a3a809cf0ca85f5dd1cb974e7babb70a306c0e7
Dimitri Kurashvili <dimakura@gmail.com>
2020-07-21 22:48:11 +0400

    Read/write strings

---
+++ chapter03/prog02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+// Reading and writing string
+
+int main() {
+  string s;
+  cin >> s;
+  cout << s << endl;
+
+  return 0;
+}
